5d82c3fc00612f6bab5b630d7ff391ea39d0f6a5
xf <xf@qq.com>
2017-11-22 22:16:40 +0800

    1

---
+++ frontend/views/layouts/main.php
@@ -0,0 +1,98 @@
+<?php
+
+/* @var $this \yii\web\View */
+/* @var $content string */
+
+use yii\helpers\Html;
+use yii\bootstrap\Nav;
+use yii\bootstrap\NavBar;
+use yii\widgets\Breadcrumbs;
+use frontend\assets\AppAsset;
+use common\widgets\Alert;
+
+AppAsset::register($this);
+?>
+<?php $this->beginPage() ?>
+<!DOCTYPE html>
+<html lang="<?= Yii::$app->language ?>">
+<head>
+    <meta charset="<?= Yii::$app->charset ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <?= Html::csrfMetaTags() ?>
+    <title><?= Html::encode($this->title) ?></title>
+    <?php $this->head() ?>
+</head>
+<body>
+<?php $this->beginBody() ?>
+
+<div class="wrap" style="border:solid 1px;">
+    <?php
+    NavBar::begin([
+        'brandLabel' => '<image class="header-logo" src="uploads/logo.png" />',
+    	//'brandOptions'=> ['style'=>''],
+        'brandUrl' => Yii::$app->homeUrl,
+        'options' => [
+            'class' => 'navbar-inverse navbar-fixed-top navbar',
+        ],
+    ]);
+    $menuItems = [
+      
+        ['label' => '关于我们', 'url' => ['/site/about']],
+        ['label' => '联系我们', 'url' => ['/site/contact']],
+    ];
+    if (Yii::$app->user->isGuest) {
+        $menuItems[] = ['label' => '注册', 'url' => ['/site/signup']];
+        $menuItems[] = ['label' => '登录', 'url' => ['/site/login']];
+    } else {
+        $menuItems[] = '<li>'
+            . Html::beginForm(['/site/logout'], 'post')
+            . Html::submitButton(
+                '退出 (' . Yii::$app->user->identity->username . ')',
+                ['class' => 'btn btn-link']
+            )
+            . Html::endForm()
+            . '</li>';
+    }
+    echo Nav::widget([
+        'options' => ['class' => 'navbar-nav navbar-right'],
+        'items' => $menuItems,
+    ]);
+    NavBar::end();
+    ?>
+    <!--<div class="left">
+        <div class="col-md-2  " >
+            <ul class="list-group">
+                <li class="list-group-item">
+                    <span class="glyphicon glyphicon-comment" aria-hidden="true"></span> 最新回复
+                </li>
+                <li class="list-group-item">
+                    11111111111111111111111
+                </li>
+            </ul>
+        </div>
+    </div>-->
+    <div class="container">
+        <?= Breadcrumbs::widget([
+            'links' => isset($this->params['breadcrumbs']) ? $this->params['breadcrumbs'] : [],
+        ]) ?>
+        <?= Alert::widget() ?>
+        <?= $content ?>
+
+    </div>
+
+</div>
+
+
+
+<footer class="footer">
+    <div class="container">
+        <p class="pull-left">&copy; xf_blog <?= date('Y') ?></p>
+
+        <p class="pull-right"><?= Yii::powered() ?></p>
+    </div>
+</footer>
+
+<?php $this->endBody() ?>
+</body>
+</html>
+<?php $this->endPage() ?>
