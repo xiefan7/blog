30a419d83156df23cb17d2842e9a2ce4413d0928
xiefan <529787995@qq.com>
2017-11-30 17:11:27 +0800

    .

--- backend/views/layouts/main.php
+++ backend/views/layouts/main.php
@@ -68,7 +68,7 @@ AppAsset::register($this);
             'links' => isset($this->params['breadcrumbs']) ? $this->params['breadcrumbs'] : [],
         ]) ?>
         <?= Alert::widget() ?>
-        <input type="text" value="<?= Yii::$app->request->baseUrl ;?>">
+        <!--<input type="text" value="<?/*= Yii::$app->request->baseUrl ;*/?>">-->
         <?= $content ?>
     </div>
 </div>
